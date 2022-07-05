3996dce77713a4560815b70f27737c6c4517c64e
[Radin afshar] <[afshar1381@gmail.com]>
2022-07-05 21:53:09 +0300

    commiting all the files

--- facial recognition/facial_recognition.py
+++ facial recognition/facial_recognition.py
@@ -1,1 +1,27 @@
+# Import standard dependencies
 import cv2
+import os
+import random
+import numpy as np
+
+# Import tensorflow dependencies - Functional API
+from matpoltlib import pyplot as plt
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
+
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus: 
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+# Setup paths
+POS_PATH = os.path.join('data', 'positive')
+NEG_PATH = os.path.join('data', 'negative')
+ANC_PATH = os.path.join('data', 'anchor')
+
+# Make the directories
+os.makedirs(POS_PATH)
+os.makedirs(NEG_PATH)
+os.makedirs(ANC_PATH)
+
